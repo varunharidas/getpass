0285ee271f5b8d2ce5eca71ba1d11c880e33e115
Varun <touchvarun@live.com>
2019-10-15 13:13:20 +0530

    GA

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React, { Component } from 'react';
 import Home from './Views/Home';
 import About from './Views/About';
 import Privacy from './Views/Privacy';
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import './App.css';
 
-const history = createBrowserHistory();
-
-
-// Initialize google analytics page view tracking
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter history={history}>
+      <BrowserRouter >
         <div className="App">
           <Route exact={true} path='/' render={() => (
             <div className="maincontainer">
